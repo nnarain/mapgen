0df72300f10a34e09b1aef6bde55893e052815ac
Natesh Narain <nnaraindev@gmail.com>
2018-04-15 16:51:57 -0400

    Changed generator parameter type to perserve the order listed in the yaml file

--- include/generator/map_generator.h
+++ include/generator/map_generator.h
@@ -20,6 +20,34 @@ public:
 	virtual std::string getName() const = 0;
 	virtual std::vector<std::string> getLayerNames() const = 0;
 
+protected:
+	ParameterValue & getParam(ParameterLoader::GeneratorParameters& params, const std::string& name)
+	{
+		for (auto& param : params)
+		{
+			if (param.first == name)
+			{
+				return param.second;
+			}
+		}
+
+		throw std::runtime_error("Could not find parameter value");
+	}
+
+	NoiseParameters& getNoise(ParameterLoader::GeneratorParameters& params, const std::string& name)
+	{
+		return getParam(params, name).param.noise;
+	}
+
+	float* getColor(ParameterLoader::GeneratorParameters& params, const std::string& name)
+	{
+		return &getParam(params, name).param.color[0];
+	}
+
+	float getFloat(ParameterLoader::GeneratorParameters& params, const std::string& name)
+	{
+		return getParam(params, name).param.value;
+	}
 };
 
 #endif  // GENERATOR_MAP_GENERATOR_H
